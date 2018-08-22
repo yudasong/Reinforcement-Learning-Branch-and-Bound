ac9a96d5809ed86ff5c954b213c6afd95e9de33f
bettier <juz083@ucsd.edu>
2018-08-22 14:14:22 -0700

    generate functions(in process)

---
+++ generateFunctions.py
@@ -0,0 +1,26 @@
+#This file will generate functions in polynomials 
+import numpy as np
+import random 
+def init(x_vector,high_degree_vector, rangeLow, rangeHigh, ): 
+	self.x_vector = x_vector 
+	self.high_degree_vector = high_degree_vector
+	self.rangeLow = rangeLow
+	self.rangeHigh = rangeHigh
+	self.sizeHigh = len(self.high_degree_vector)
+
+#This means that coefficient for the highest one is not 0 at least 
+def highestVar():
+	self.x_vector
+def randomNonzero(): 
+	highCo = np.random.randint(low=self.rangeLow,high=self.rangeHigh,size=self.sizeHigh)
+	allowed_values = list(range(self.rangeLow,self.rangeHigh))
+	allowed_values.remove(0)
+	for i in range(self.sizeHigh): 
+		while highCo[i] == 0: 
+			highCo[i] = random.randint()
+def generateFunction():
+def stringRepresent(): 
+def generatePoly(): 
+
+
+
