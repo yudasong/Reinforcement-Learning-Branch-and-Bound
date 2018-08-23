42ce1e72d1901de37377f7381437a35291d90dc6
bettier <juz083@ucsd.edu>
2018-08-22 19:02:40 -0700

    add the pure

--- pureMCTS/nonlinear.py
+++ pureMCTS/nonlinear.py
@@ -1,8 +1,29 @@
-import 
+# the nonlinear class to solve problems 
+import pyibex as pi
 import generateFunctions as generator
+import numpy as np 
 
 class Nonlinear(): 
-    def __init__(self, function, interval_vector, output_range):
+    def __init__(self, function, input_range, output_range):
         self.function = function
-        self.interval_vector = interval_vector
+        self.input_range = input_range
         self.output_range = output_range
+
+    #return a matrix 
+    def getRootMatrix(self,input_range):
+        num_row = len(input_range)*2 
+        num_col = 2 
+        matrix = np.zeros((num_row,num_col))
+
+        for i in range(num_row):
+            lower = input_range[i][0]
+            upper = input_range[i][1]
+
+            matrix[i,0] = lower
+            matrix[i,1] = upper 
+        return matrix
+    
+    
+
+
+
