89305e3296501783b46e3bfb33565607baf5eb6b
bettier <juz083@ucsd.edu>
2018-08-24 16:41:43 -0700

    add generator into optimization

--- testOptimization.py
+++ testOptimization.py
@@ -10,23 +10,24 @@ import random
 class optimization():
 
 	def func(self, x):
+		#print(x)
 		#f = np.power(x[0],2) + np.power(x[1],2) + np.power(x[2],2)
-		f = 2 * np.power(x[0],3) * np.power(x[1],3) * np.power(x[2],3) - 5 * x[0] * x[2] - 5 * np.power(x[1],4) * x[2] - 2 * np.power(x[0],3) * np.power(x[2],5)
-		#p = generator(["x1","x2","x3"],[3,4,5],-5,5,4)
-		#p.randomPara()
-		#f = p.generateFunction(coe, degree_matrix)
+		#f = 2 * np.power(x[0],3) * np.power(x[1],3) * np.power(x[2],3) - 5 * x[0] * x[2] - 5 * np.power(x[1],4) * x[2] - 2 * np.power(x[0],3) * np.power(x[2],5)
+		p = generator(x,[3,4,5],-5,5,4)
+		p.randomPara()
+		f = p.generateFunction(p.coe, p.degree_matrix)
 		return f
 
 	def basinhopping_opt(self):
 		#minimizer_kwargs = {"method":"L-BFGS-B", "jac":True}
-		x0 = [-1000000000,-1000000000,-1000000000]
+		x0 = [-8,-1000000000,-1000000000]
 		#func: function to be optimized 
 		#x0: initial guess 
 		#niter: the number of basin-hopping iterations 
 		
-		result = basinhopping(self.func, x0)
-		#print(self.func)
-		print("global minimum: x = [%.4f, %.4f, %.4f], f(ini_guess) = %.4f" % (result.x[0],result.x[1],result.x[2], result.fun))
+		result = basinhopping(self.func, x0,niter=100)
+		print(self.func)
+		print("global minimum: x = [%.4f, %.4f, %.4f], f(ini_guess) = %.4f" % (result.x[0],result.x[1],result.x[2],result.fun))
 		#global minimum: x = [900855863.9998, -1899308697.5270, 3069649694.9555]
 		#f(ini_guess) = -289767082547134003478347253256009015498519474034963560230047619738747771411939459072.0000
 
